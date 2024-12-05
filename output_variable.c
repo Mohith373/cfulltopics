dabba1602525aff0995a7e405677ae3f977b18a4
Mohith373 <mohithias24@gmail.com>
2024-12-05 19:51:59 +0530

    Add files via upload

---
+++ output_variable.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+        int two=2, three=3;
+        int result = two * three;
+        printf("The result is: %d * %d = %d\n", two, three, result);
+        printf("%d",result);
+    
+        return 0;
+}
